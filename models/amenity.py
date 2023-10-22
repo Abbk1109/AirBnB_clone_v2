58b5d5f82b354278d4e7bd6824fbe42582a7d9bb
Abbk1109 <abbk1983@gmail.com>
2023-10-22 03:41:16 -0700

    fix shebang

--- models/amenity.py
+++ models/amenity.py
@@ -1,24 +1,21 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import DateTime
-from sqlalchemy.sql.schema import Column, ForeignKey
-from sqlalchemy.sql.sqltypes import String, Integer
-
 from os import getenv
-from sqlalchemy.orm import relationship, backref
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    class Amenity(BaseModel, Base):
-        """Class Ameniy"""
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary='place_amenity',
-                                       back_populates='amenities')
-else:
-    class Amenity(BaseModel):
-        """Class Ameniy"""
+    else:
+        name = ""
 
-    name = ""
-
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
